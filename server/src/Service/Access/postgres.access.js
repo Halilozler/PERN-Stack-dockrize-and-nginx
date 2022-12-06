91aed826b9a8d8e6b4dfa67ad449dd71e44f75cb
Halil Özler <halil_ozler@hotmail.com>
2022-12-06 19:26:01 +0300

    first commit

--- server/src/Service/Access/postgres.access.js
+++ server/src/Service/Access/postgres.access.js
@@ -2,15 +2,19 @@ const { Client } = require("pg");
 const pg_config = require("../../Config/postgre.config");
 
 const pg_connect = new Promise(async(resolve, reject)=> {
-    global.db = await new Client(pg_config);
+    try {
+        global.db = await new Client(pg_config);
 
-    global.db.connect(function(err) {
-        if(err){
-            console.log("database connect error");
-            reject(err);
-        }
-        resolve();
-    })
+        global.db.connect(function(err) {
+            if(err){
+                console.log("database connect error");
+                reject(err);
+            }
+            resolve();
+        })
+    } catch (err) {
+        reject(err);
+    }
 }) 
 
 module.exports = pg_connect;
--- server/src/index.js
+++ server/src/index.js
@@ -5,15 +5,28 @@ const pg_connect = require("./Service/Access/postgres.access");
 
 const initialize = async() => {
     //database connection and open port
-
-    pg_connect.then(() => {
-        app.listen(port, () => {
-            console.log("database connect");
-            console.log(`Example app listening at http://localhost:${port}`);
-        })
-    }).catch((err) => {console.error(err);})
-
-    
+    let dbconnect = 0;
+    const interval = setInterval(async() => {
+        try {
+            if(dbconnect > 9){
+                console.log("Failed to connect to database, shutting down");
+                clearInterval(interval);
+                process.kill();
+            }
+            await pg_connect.then(() => {
+                app.listen(port, () => {
+                    console.log("database connect");
+                    console.log(`Example app listening at http://localhost:${port}`);
+                    clearInterval(interval);
+                }).catch((err) => { throw new Error(err) })
+            })
+        } catch (err) {
+            dbconnect++;
+            console.log(`${dbconnect}. deneme`);
+            console.log(err);
+        }
+    }, 1500)
 }
+
 
 initialize();
